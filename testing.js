5f392738c9b7731f0a249b0eef629d25e157609b
Gyutaro <80272391+Doma-byte@users.noreply.github.com>
2023-07-06 10:47:29 +0530

    Update testing.js

--- testing.js
+++ testing.js
@@ -4,7 +4,3 @@ const parser = new ReadlineParser()
 port.pipe(parser)
 parser.on('data', console.log)
 port.write('ROBOT PLEASE RESPOND\n')
-// ROBOT ONLINE
-
-// Creating the parser and piping can be shortened to
-// const parser = port.pipe(new ReadlineParser())
